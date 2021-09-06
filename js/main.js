8d412f58c036cb35d32f678692f21d49f47e15f7
kukuvz <antonslayer@gmail.com>
2021-09-06 18:25:23 +0300

    adaptive 1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(function() {
-  var mixer = mixitup('.gallery__content');
+
 
   $('.slider-blog__inner').slick({
     dots: true,
     arrows: false,
   });
+
+  $('.menu__btn').on('click', function() {
+    $('.header__top').toggleClass('header__top--active');
+  });
+
+  var mixer = mixitup('.gallery__content');
 });
