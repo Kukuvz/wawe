640e916af44e0d7d5bcaafe2e8b5e28ba8c3c72c
Anton Gaivoronskiy <gaivoronskiy.anton.i@gmail.com>
2021-09-11 16:36:17 +0300

    smooth scroll&sticky header

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 $(function() {
 
+  //Smooth scroll
+  $(".menu a, .logo").on("click", function (e) {
+		e.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+  //slider
   $('.slider-blog__inner').slick({
     dots: true,
     arrows: false,
   });
 
-  $('.menu__btn').on('click', function() {
+  //burger menu
+  $('.menu__btn,  .menu a').on('click', function() {
     $('.header__top').toggleClass('header__top--active');
   });
 
+  //gallery mixer
   var mixer = mixitup('.gallery__content');
 });
